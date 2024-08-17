91e1bd34c00ad3e96539504341dd3d8ce879c50d
hayato_52b <a99246780@icloud.com>
2024-08-17 11:09:23 +0900

    flashメッセージ用のViewを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_type_class(type)
+    case type.to_s
+    when "notice" then "bg-green-400"
+    when "alert" then "bg-red-400"
+    else "bg-yellow-300"
+    end
+  end
 end
