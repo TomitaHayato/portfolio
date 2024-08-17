e4bf4217e127ff1a3610fb1ae2d864e6aa57fb93
hayato_52b <a99246780@icloud.com>
2024-08-17 12:55:01 +0900

    ユーザー新規登録ページを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, presence: true, uniqueness: true
-  validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
   validates :name, presence: true
 
   enum role: { admin: 0, general: 1 }
-
-  def initialize_user
-    role = "general"
-    complete_routines_count = 0
-  end
 end
