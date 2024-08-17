cc1fe623281b6547ab041c5ac2a0eef51c4d9094
hayato_52b <a99246780@icloud.com>
2024-08-17 14:25:40 +0900

    ログインが必要なページにsorceryのrequire_loginメソッドを使用する

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,3 +1,12 @@
 class TopPagesController < ApplicationController
+  skip_before_action :require_login
+  before_action :login_check
+
   def index; end
+
+  private
+
+  def login_check
+    redirect_to my_pages_path if current_user
+  end
 end
