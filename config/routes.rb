c1e995001506fbe37d2d935931984698acad79d4
hayato_52b <a99246780@icloud.com>
2024-08-18 20:48:03 +0900

    Routinesコントローラの作成、new, createアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :routines, only: %i[ new create ]
 end
