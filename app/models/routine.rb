b32e5fecbd9af232362d1753c1329a663063c026
hayato_52b <a99246780@icloud.com>
2024-08-20 15:58:50 +0900

    scopeメソッドを削除

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -3,6 +3,4 @@ class Routine < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 500 }
-
-  scope :active, -> { find_by(is_active: true) }
 end
