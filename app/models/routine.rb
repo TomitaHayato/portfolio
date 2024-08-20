7d5d5e10944ca4de3f0cf4ffa454aebfee0cbd1c
hayato_52b <a99246780@icloud.com>
2024-08-20 18:26:09 +0900

    Tasks tableをDBに追加する

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,5 +1,6 @@
 class Routine < ApplicationRecord
   belongs_to :user
+  has_many :tasks, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 500 }
