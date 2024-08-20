c37b36bf534ceb548038039ab22df07381f3c2c8
hayato_52b <a99246780@icloud.com>
2024-08-20 14:30:56 +0900

    routines/actives_controllerの修正＋ルーティン一覧画面から遷移

--- app/controllers/routines/actives_controller.rb
+++ app/controllers/routines/actives_controller.rb
@@ -1,12 +1,12 @@
 class Routines::ActivesController < ApplicationController
 
-  def create
-    # 現在実践中のルーティンを実践前状態に変更
-    routine_active_now = Routine.find_by(is_active: true)
-    routine_active_now.update!(is_active: false) if routine_active_now
-
+  def update
     routine = Routine.find(params[:routine_id])
-    routine.update!(is_active: true)
+    routine_active_now = Routine.find_by(is_active: true)
+    unless routine == routine_active_now
+        routine_active_now.update!(is_active: false) if routine_active_now
+        routine.update!(is_active: true)
+    end
     redirect_to routines_path
   end
 end
