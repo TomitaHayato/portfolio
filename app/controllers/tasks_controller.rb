58aacbfac576e9133cd844e10c2406a9b6e4a076
hayato_52b <a99246780@icloud.com>
2024-08-21 13:49:03 +0900

    tasksコントローラにupdateアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_routine
   before_action :params_time_to_second
-
+  
   def create
-    @routine = current_user.routines.find(params[:routine_id])
     @task = @routine.tasks.new(task_params)
     if @task.save
       flash[:notice] = "タスクを追加しました"
@@ -13,7 +13,21 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "taskを更新しました"
+      redirect_to routine_path(@routine)
+    else
+      @tasks = @routine.tasks.order(created_at: :desc)
+      render template: "routines/show", status: :unprocessable_entity
+  end
+
   private
+
+  def set_routine
+    @routine = current_user.routines.find(params[:routine_id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :estimated_time_in_second)
