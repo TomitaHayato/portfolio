b2ca1d29f340fbc4f7af160228d0c8a0bd43c7f5
hayato_52b <a99246780@icloud.com>
2024-08-21 14:01:35 +0900

    タスク情報をマイページに表示

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,5 +1,5 @@
 class MyPagesController < ApplicationController
   def index
-    @routine = current_user.routines.find_by(is_active: true)
+    @routine = current_user.routines.includes(:tasks).find_by(is_active: true)
   end
 end
