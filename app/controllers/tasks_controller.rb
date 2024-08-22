12a344c8b2216d95635d6e95d10a7e3a62f244f6
hayato_52b <a99246780@icloud.com>
2024-08-22 21:38:34 +0900

    タスク削除機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :params_time_to_second
+  before_action :params_time_to_second, only: %i[ create update ]
+  before_action :set_task_and_routine, only: %i[ update destroy ]
   
   def create
     @routine = current_user.routines.find(params[:routine_id])
@@ -14,8 +15,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
-    @routine = @task.routine
     if @task.update(task_params)
       flash[:notice] = "taskを更新しました"
       redirect_to routine_path(@routine)
@@ -25,7 +24,18 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task.destroy!
+    flash[:notice] = "タスクを削除しました。(タスク名：#{@task.title})"
+    redirect_to routine_path(@routine), status: :see_other
+  end
+
   private
+
+  def set_task_and_routine
+    @task = Task.find(params[:id])
+    @routine = @task.routine
+  end
 
   def task_params
     params.require(:task).permit(:title, :estimated_time_in_second)
