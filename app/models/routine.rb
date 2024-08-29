e8a6ff7ace9f92b375050b19ab604b0e10c3fb77
hayato_52b <a99246780@icloud.com>
2024-08-29 21:55:22 +0900

    ルーティンのtitleの最大文字数を50に変更

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,7 +2,7 @@ class Routine < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 25 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 500 }
 
   scope :posted, -> { where(is_posted: true) }
