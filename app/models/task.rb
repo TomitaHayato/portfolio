eb5e3c0e03f5ad7e64aa06a7354e922280dbf4d7
hayato_52b <a99246780@icloud.com>
2024-08-29 21:54:27 +0900

    タスク・ルーティンのタイトルの最大文字数を25設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :routine
   acts_as_list scope: :routine
 
-  validates :title, presence: true, length: { maximum: 50 }
+  validates :title, presence: true, length: { maximum: 25 }
   validates :estimated_time_in_second, presence: true
 
   def estimated_time
