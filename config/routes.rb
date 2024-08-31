e18e86f3065eb55533c8b723ca8afd7793ae9244
hayato_52b <a99246780@icloud.com>
2024-09-01 01:12:03 +0900

    route.rbのlint修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "top_pages#index"
-  resources :users, only: %i[ new create show edit update ]
-  resources :my_pages, only: %i[ index ]
+  # Defines the root path route ('/')
+  root 'top_pages#index'
+  resources :users, only: %i[new create show edit update]
+  resources :my_pages, only: %i[index]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :routines do
-    resources :tasks, only: %i[ create update destroy ], shallow: true
-    resources :copies, only: %i[ create ], module: :routines
-    resources :plays, only: %i[ create update show ], param: :routine_id, module: :routines, shallow: true
+    resources :tasks, only: %i[create update destroy], shallow: true
+    resources :copies, only: %i[create], module: :routines
+    resources :plays, only: %i[create update show], param: :routine_id, module: :routines, shallow: true
   end
 
   namespace :routines, path: 'routine' do
-    resources :actives, only: %i[ update ], param: :routine_id
-    resources :posts, only: %i[ index update ], param: :routine_id
-    resources :finishes, only: %i[ index ]
+    resources :actives, only: %i[update], param: :routine_id
+    resources :posts, only: %i[index update], param: :routine_id
+    resources :finishes, only: %i[index]
   end
 
   namespace :tasks do
-    resources :move_highers, only: %i[ update ], param: :task_id
-    resources :move_lowers, only: %i[ update ], param: :task_id
+    resources :move_highers, only: %i[update], param: :task_id
+    resources :move_lowers, only: %i[update], param: :task_id
   end
 end
