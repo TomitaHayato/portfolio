97d52c4470a0346fa9bd9ee698296a3a4ded70e8
hayato_52b <a99246780@icloud.com>
2024-09-05 16:50:32 +0900

    ドラッグ&ドロップでタスクの並べ替えを行う機能を実装

--- app/javascript/sortable.js
+++ app/javascript/sortable.js
@@ -1,12 +1,26 @@
 import Sortable from 'sortablejs';
 
-document.addEventListener("turbo:load", function(event) {
+document.addEventListener('turbo:load', function(event) {
   const el = document.querySelector('#task-index');
   if (el != null) {
     Sortable.create(el, {
       forceFallback: true,
-      fallbackClass: "sortable-fallback",
-      direction: 'vertical'
+      fallbackClass: 'sortable-fallback',
+      direction: 'vertical',
+      onUpdate: function (event) {
+        const itemElement = event.item;
+        const taskId = itemElement.querySelector('.task-id').textContent;
+        const oldIndex = event.oldIndex;
+        const newIndex = event.newIndex;
+
+        const oldIndexForm = itemElement.querySelector(`#form-old-index-${taskId}`);
+        const newIndexForm = itemElement.querySelector(`#form-new-index-${taskId}`);
+        const submitBtn = itemElement.querySelector(`#sort-btn-${taskId}`);
+
+        oldIndexForm.value = oldIndex;
+        newIndexForm.value = newIndex;
+        submitBtn.click();
+      }
     });
   }
 });
