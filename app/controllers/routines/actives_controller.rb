49ba4643e2a1ac46b13ea5c8b039c88932bbfacd
hayato_52b <a99246780@icloud.com>
2024-09-13 11:48:59 +0900

    ルーティン実践ボタンのajax化

--- app/controllers/routines/actives_controller.rb
+++ app/controllers/routines/actives_controller.rb
@@ -1,11 +1,10 @@
 class Routines::ActivesController < ApplicationController
   def update
-    routine = current_user.routines.find(params[:routine_id])
-    routine_active_now = current_user.routines.find_by(is_active: true)
-    unless routine == routine_active_now
-      routine_active_now&.update!(is_active: false)
-      routine.update!(is_active: true)
+    @routine = current_user.routines.find(params[:routine_id])
+    @routine_active_now = current_user.routines.find_by(is_active: true)
+    unless @routine == @routine_active_now
+      @routine_active_now&.update!(is_active: false)
+      @routine.update!(is_active: true)
     end
-    redirect_to routines_path
   end
 end
