ae4c7e9d06be5d9aefd6e15dcf1981695f60f933
hayato_52b <a99246780@icloud.com>
2024-09-14 01:11:27 +0900

    獲得した経験値をマイページに表示する機能を追加

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,5 +1,21 @@
 class MyPagesController < ApplicationController
   def index
     @routine = current_user.routines.includes(:tasks).find_by(is_active: true)
+    @experience_data_all = make_tag_point_hash(current_user.user_tag_experiences.includes(:tag))
+    @experience_data_month = make_tag_point_hash(current_user.user_tag_experiences.includes(:tag).recent_one_month)
+    @experience_data_week = make_tag_point_hash(current_user.user_tag_experiences.includes(:tag).recent_one_week)
+    # p "all:#{@experience_data_all} month#{@experience_data_month} week#{@experience_data_week}"
+  end
+
+  private
+
+  def make_tag_point_hash(experience_collection)
+    tag_point_hash = Hash.new
+    experience_collection.each do |experience|
+      tag = experience.tag
+      tag_point_hash[tag.name] ||= 0
+      tag_point_hash[tag.name] += experience.experience_point
+    end
+    tag_point_hash
   end
 end
--- app/models/user_tag_experience.rb
+++ app/models/user_tag_experience.rb
@@ -3,4 +3,7 @@ class UserTagExperience < ApplicationRecord
   belongs_to :tag
 
   validates :experience_point, presence: true
+
+  scope :recent_one_month, -> { where("created_at >= ?", 1.month.ago) }
+  scope :recent_one_week, -> { where("created_at >= ?", 1.week.ago) }
 end
