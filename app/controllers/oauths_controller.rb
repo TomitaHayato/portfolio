3329a96dd506f9ae429c65e13d4ffcb130357cac
hayato_52b <a99246780@icloud.com>
2024-09-27 02:35:25 +0900

    Oauthsコントローラの作成＋ユーザー作成時に独自のメールアドレスを登録する処理を追加

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,48 @@
+require 'securerandom'
+require 'digest'
+
+class OauthsController < ApplicationController
+  skip_before_action :require_login, raise: false
+      
+  def oauth
+    login_at(auth_params[:provider])
+  end
+      
+  def callback
+    provider = auth_params[:provider]
+    if @user = login_from(provider)
+      redirect_to my_pages_path, :notice => "ログインしました！"
+    else
+      begin
+        @user = create_from(provider)
+
+        # create_fromの時点では、emailカラムにLINEのuserIdが格納されている。
+        # 以下の処理でメールアドレスからLINEのuserIdが推測されないようにする。
+        unique_email = generate_unique_email_address(@user.email)
+        @user.update!(email: "#{unique_email}@email")
+
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
+      rescue
+        redirect_to root_path, :alert => "Failed to login from #{provider.titleize}!"
+      end
+    end
+  end
+  
+  private
+
+  def auth_params
+    params.permit(:code, :provider, :error, :state)
+  end
+
+  # 引数に渡した文字列からハッシュ値を生成する
+  def generate_digest_hash(letter)
+    Digest::SHA256.hexdigest(letter)
+  end
+
+  # 渡されたメールアドレスから、一意のメールアドレスを生成する
+  def generate_unique_email_address(email_address)
+    generate_digest_hash(email_address) + SecureRandom.uuid
+  end
+end
