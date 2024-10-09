ac1d7b93ad4921077d02fb85adc6c9fdb7a8f8fa
hayato_52b <a99246780@icloud.com>
2024-10-10 03:57:29 +0900

    conditionカラム名を修正＋初期データを作成

--- db/migrate/20241009114028_create_rewards.rb
+++ db/migrate/20241009114028_create_rewards.rb
@@ -2,7 +2,7 @@ class CreateRewards < ActiveRecord::Migration[7.0]
   def change
     create_table :rewards do |t|
       t.string :name,     null: false
-      t.string :codition, null: false
+      t.string :condition, null: false
       t.string :description
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,36 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 tag_names_list = ['運動・健康', '美容', '趣味', '学習', '仕事・タスク', '自己投資', '日課']
-tag_names_list.each do |tag_name|
-  Tag.create!(
-    name: tag_name
+if Tag.all.size == 0
+  tag_names_list.each do |tag_name|
+    Tag.create!(
+      name: tag_name
+    )
+  end
+end
+
+if Reward.all.size == 0
+  Reward.create!(
+    name: "はじまりの一歩！",
+    condition: "completed_routine_1?",
+    description: "ルーティンを1回クリアする"
+  )
+
+  Reward.create!(
+    name: "小さな達成者",
+    condition: "completed_routines_3?",
+    description: "ルーティンを3回クリアする"
+  )
+
+  Reward.create!(
+    name: "若葉の成長",
+    condition: "get_experiences_10?",
+    description: "経験値を10獲得する"
+  )
+
+  Reward.create!(
+    name: "朝の森の案内人",
+    condition: "post_routine_1?",
+    description: "ルーティンを1つ投稿する"
   )
 end
