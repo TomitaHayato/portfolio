49c727f40c85b625c42c17a2cd6a6d5592717f9b
hayato_52b <a99246780@icloud.com>
2024-10-09 18:50:04 +0900

    メール本文の微調整

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,6 +9,6 @@ class UserMailer < ApplicationMailer
     @user = User.find(user.id)
     @url  = edit_password_reset_url(@user.reset_password_token)
     mail(:to => user.email,
-         :subject => "Your password has been reset")
+         :subject => "[Morning Forest] パスワードリセット申請")
   end
 end
