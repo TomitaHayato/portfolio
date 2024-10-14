d34d929f1e3777bedb30869873d5d93d1785da47
hayato_52b <a99246780@icloud.com>
2024-10-14 16:06:34 +0900

    トップページのsystem specを完成

--- spec/system/top_pages_spec.rb
+++ spec/system/top_pages_spec.rb
@@ -8,18 +8,56 @@ RSpec.describe "TopPages", type: :system do
       before do
         visit root_path
       end
+      
+      describe 'トップページのヘッダーに関するテスト' do
+        it 'トップページのヘッダーにログインボタンが表示される' do
+          expect(page).to have_link('ログイン')
+          click_link 'ログイン'
+          expect(page).to have_current_path(login_path)
+        end
 
-      it 'トップページにログイン前のヘッダーが表示されている' do
-        expect(page).to have_link('ログイン')
-        expect(page).to have_link('header-logo')
+        it 'トップページのヘッダーに新規登録ボタンが表示される' do
+          expect(page).to have_link('新規登録')
+          click_link '新規登録'
+          expect(page).to have_current_path(new_user_path)
+        end
+
+        it 'トップページのヘッダーにヘッダーロゴが表示される' do
+          expect(page).to have_link('header-logo')
+          find('#header-logo').click
+          expect(page).to have_current_path(root_path)
+        end
+
+        it 'トップページのヘッダーにお試しログインボタンが表示される' do
+          expect(page).to have_link('お試し')
+          click_link 'お試し'
+          expect(page).to have_current_path(my_pages_path)
+          expect(page).to have_content('ゲストログインしました。')
+        end
       end
 
-      it 'トップページにアプリタイトルが表示されている' do
-        expect(page).to have_selector('h1', text: 'Morning')
+      describe 'トップページのメインコンテンツに関するテスト' do
+        it 'トップページにアプリタイトルが表示されている' do
+          expect(page).to have_selector('h1', text: 'Morning')
+        end
+
+        it 'トップページにアプリの使い方が表示されている' do
+          expect(page).to have_selector('h1', text: 'アプリの使い方')
+        end
       end
 
-      it 'トップページにアプリの使い方が表示されている' do
-        expect(page).to have_selector('h1', text: 'アプリの使い方')
+      describe 'トップページのフッターに関するテスト' do
+        it 'トップページのフッターにプライバシーポリシーリンクが表示されている' do
+          expect(page).to have_link('プライバシーポリシー')
+          click_link('プライバシーポリシー')
+          expect(page).to have_current_path(policy_path)
+        end
+
+        it 'トップページのフッターにプライバシーポリシーリンクが表示されている' do
+          expect(page).to have_link('利用規約')
+          click_link('利用規約')
+          expect(page).to have_current_path(terms_path)
+        end
       end
     end
   end
