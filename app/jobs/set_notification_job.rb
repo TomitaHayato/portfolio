b6776a5c184357190ba3993cc47d9675a19b0567
hayato_52b <a99246780@icloud.com>
2024-10-25 17:10:02 +0900

    LINE Push通知処理を、ルーティンの通知設定をLINEにしている場合のみ実行するように変更

--- app/jobs/set_notification_job.rb
+++ app/jobs/set_notification_job.rb
@@ -1,15 +1,20 @@
 class SetNotificationJob < ApplicationJob
   queue_as :default
 
+  # 全てのLINE登録済みユーザーに対し、ルーティンの開始時間にLINE Push通知を送信するJobを呼び出す
   def perform()
     time_now = Time.current
+    
     User.includes(:routines, :authentications).each do |user|
       user_line_authentication = user.authentications.find_by(provider: 'line', user_id: user.id)
       next unless user_line_authentication # UserがLineを介して登録していない場合はスキップ
 
       uid = user_line_authentication.uid
       active_routine = user.routines.find_by(is_active: true)
-      perform_line_notification_job(active_routine, uid, time_now) if active_routine
+      next unless active_routine
+
+      # 「ユーザーが実践中のルーティンの通知設定がline」の場合 => メソッド呼び出し
+      perform_line_notification_job(active_routine, uid, time_now) if active_routine.notification == "line"
     end
   end
 
