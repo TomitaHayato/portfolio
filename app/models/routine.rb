c69efe9b9623dba0789f2d0556503bc913f9beda
hayato_52b <a99246780@icloud.com>
2024-11-10 15:08:38 +0900

    modelファイルのコード整理

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,17 +1,17 @@
 class Routine < ApplicationRecord
   belongs_to :user
   has_many :tasks, -> { order(position: :asc) }, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :liked_users, through: :likes, source: :user
+  has_many :likes                              , dependent: :destroy
+  has_many :liked_users                        , through:   :likes  , source: :user
 
-  validates :title, presence: true, length: { maximum: 25 }
+  validates :title      , length: { maximum: 25  }, presence: true
   validates :description, length: { maximum: 500 }
 
   enum notification: { no: 0, line: 1 }
 
-  scope :posted,   ->           { where(is_posted: true) }
+  scope :posted,   ->           { where(is_posted: true)  }
   scope :unposted, ->           { where(is_posted: false) }
-  scope :my_post,  ->(user_id)  { where(user_id: user_id) }
+  scope :my_post,  ->(user_id)  { where(user_id:   user_id) }
   scope :official, ->           { joins(:user).where(users: { role: 'admin' }) }
   scope :general,  ->           { joins(:user).where(users: { role: 'general' }) }
   scope :liked,    ->(user_id)  { joins(:likes).where(likes: { user_id: user_id }) }
@@ -28,7 +28,7 @@ class Routine < ApplicationRecord
 
   def total_estimated_time
     result          = second_to_time_string(all_task_estimated_time)
-    result[:hour]   = "0#{result[:hour]}" if result[:hour] < 10
+    result[:hour]   = "0#{result[:hour]}"   if result[:hour]   < 10
     result[:minute] = "0#{result[:minute]}" if result[:minute] < 10
     result[:second] = "0#{result[:second]}" if result[:second] < 10
     result
@@ -38,10 +38,9 @@ class Routine < ApplicationRecord
   def self.search(user_word)
     return all unless user_word
 
-    user_words = user_word.split(' ')
-
+    user_words   = user_word.split(' ')
     search_query = user_words.map{ '(routines.title LIKE ? OR routines.description LIKE ?)' }.join(' AND ')
-    like_values = []
+    like_values  = []
     user_words.each do |word|
       2.times{ like_values << "%#{word}%" }
     end
@@ -72,16 +71,16 @@ class Routine < ApplicationRecord
   
   # 投稿の並べ替え処理
   def self.sort_posted(column, direction)
-    column = "posted_at" if column.blank? 
-    direction = "desc" if direction.blank? 
+    column    = "posted_at" if column.blank? 
+    direction = "desc"      if direction.blank? 
     
     order_by(column, direction)
   end
 
   # ルーティン一覧の並べ替え
   def self.sort_routine(column, direction)
-    column = "created_at" if column.blank? 
-    direction = "desc" if direction.blank? 
+    column    = "created_at" if column.blank? 
+    direction = "desc"       if direction.blank? 
 
     order_by(column, direction)
   end
@@ -89,13 +88,13 @@ class Routine < ApplicationRecord
   private
 
   def second_to_time_string(time_in_second)
-    hour = time_in_second / 3600
+    hour            = time_in_second / 3600
     time_in_second -= hour * 3600
 
-    minute = time_in_second / 60
+    minute          = time_in_second / 60
     time_in_second -= minute * 60
 
-    second = time_in_second
+    second          = time_in_second
 
     { hour:, minute:, second: }
   end
@@ -111,7 +110,7 @@ class Routine < ApplicationRecord
   end
 
   def self.order_by(column, direction)
-    column_sym = column.to_sym
+    column_sym    = column.to_sym
     direction_sym = direction.to_sym
 
     order(column_sym => direction_sym)
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :routine
-  has_many :task_tags, dependent: :destroy
-  has_many :tags, through: :task_tags
+  has_many   :task_tags, dependent: :destroy
+  has_many   :tags     , through:   :task_tags
 
   acts_as_list scope: :routine
 
-  validates :title, presence: true, length: { maximum: 25 }
+  validates :title,
+            presence: true,
+            length: { maximum: 25 }
   validates :estimated_time_in_second,
             presence: true,
             numericality: {
@@ -15,8 +17,8 @@ class Task < ApplicationRecord
             }
 
   def estimated_time
-    result = second_to_time_string(estimated_time_in_second)
-    result[:hour] = "0#{result[:hour]}" if result[:hour] < 10
+    result          = second_to_time_string(estimated_time_in_second)
+    result[:hour]   = "0#{result[:hour]}"   if result[:hour]   < 10
     result[:minute] = "0#{result[:minute]}" if result[:minute] < 10
     result[:second] = "0#{result[:second]}" if result[:second] < 10
     result
@@ -25,11 +27,14 @@ class Task < ApplicationRecord
   private
 
   def second_to_time_string(time_in_second)
-    hour = time_in_second / 3600
+    hour            = time_in_second / 3600
     time_in_second -= hour * 3600
-    minute = time_in_second / 60
+
+    minute          = time_in_second / 60
     time_in_second -= minute * 60
-    second = time_in_second
+    
+    second          = time_in_second
+
     { hour:, minute:, second: }
   end
 end
