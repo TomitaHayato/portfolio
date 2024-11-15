dea15f9a2a10eac23c093829fa116eea2065925f
hayato_52b <a99246780@icloud.com>
2024-11-15 16:46:54 +0900

    ADD: routines_finishesコントローラのindexアクションにレベルアップ処理を追加

--- app/controllers/routines/finishes_controller.rb
+++ app/controllers/routines/finishes_controller.rb
@@ -1,11 +1,14 @@
 class Routines::FinishesController < ApplicationController
   skip_before_action :playing_task_sesison_reset
-  before_action :block_if_no_session
+  before_action      :block_if_no_session
 
   def index
-    current_user.complete_routines_count += 1
-    current_user.save!
-    user_get_tag_experiences(session[:experience_log]) # 経験値情報をuser_tag_experiencesテーブルに保存
+    current_user.add_complete_routines_count
+    # 経験値情報をuser_tag_experiencesテーブルに保存
+    user_get_tag_experiences(session[:experience_log])
+                                                # レベルアップ処理
+    flash.now[:notice] = 'レベルアップしました！！' if current_user.level_up_check
+    
     @tag_point_hash = make_tag_point_hash(session[:experience_log])
   end
 
@@ -22,15 +25,17 @@ class Routines::FinishesController < ApplicationController
     end
   end
 
-  # { タグの名前: 獲得した経験値 }の形式のハッシュを作成する。
+  # { Tag名: 獲得Exp }を作成
   # viewでどのタグの経験値をどのくらい獲得したかを表示するために使用。
   def make_tag_point_hash(experience_log)
     tag_point_hash = Hash.new
-    tags_hash = Tag.all.index_by(&:id) # key=id, value=Tagモデルインスタンスとなるハッシュを作成。ハッシュにしてクエリ削減
+    tags_hash      = Tag.all.index_by(&:id) # key=id, value=Tagモデルインスタンスとなるハッシュを作成。ハッシュにしてクエリ削減
+    
     experience_log.each do |tag_id, point|
-      tag = tags_hash[tag_id.to_i]
+      tag                      = tags_hash[tag_id.to_i]
       tag_point_hash[tag.name] = point
     end
+
     tag_point_hash
   end
 end
