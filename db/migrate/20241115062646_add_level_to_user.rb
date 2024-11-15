1b430e3eb8fdd150627c69bed8fa3fc1801aa443
hayato_52b <a99246780@icloud.com>
2024-11-15 15:57:35 +0900

    levelカラムにNOT_NULL制約を追加

--- db/migrate/20241115062646_add_level_to_user.rb
+++ db/migrate/20241115062646_add_level_to_user.rb
@@ -1,5 +1,5 @@
 class AddLevelToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :level, :integer, default: 1
+    add_column :users, :level, :integer, default: 1, null: false
   end
 end
