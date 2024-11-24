badd0a300dca7f19f2f3d808ec9270ce57818fd4
hayato_52b <a99246780@icloud.com>
2024-11-24 20:33:23 +0900

    Style: ユーザーアイコンの表示を修正

--- app/javascript/user_icon_preview.js
+++ app/javascript/user_icon_preview.js
@@ -15,7 +15,6 @@ document.addEventListener('turbo:load', function(event) {
       // 新しい画像のimg要素を生成する
       const newImg = document.createElement('img');
       newImg.id = 'preview';
-      newImg.className = 'rounded-circle mb-3 mx-auto size-[150px]';
       newImg.src = URL.createObjectURL(file);
 
       // 生成した要素をimgZoneの子要素としてHTMLに追加する
