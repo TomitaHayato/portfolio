5094e4fc98b047f53305e1cbf4e53a9514ce32ad
hayato_52b <a99246780@icloud.com>
2024-11-28 23:33:40 +0900

    ADD: メールでリマインダーを送信できる機能を実装

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 #TODO: メールのViewを追加
 class NotificationMailer < ApplicationMailer
   # 呼び出し：NotifcationMailer.with(user: user).notify_start.deliver_later
-  def notify_start
+  def notify_email
     # ユーザーを取得=>ユーザーのemailにマイページへのURLを載せる
     @user = params[:user]
     @url  = my_pages_url
