ff5ceb5b0a3cb2ce80a854db56debe0f1424d6b4
hayato_52b <a99246780@icloud.com>
2024-12-04 04:24:26 +0900

    UPdate: edit_routine_pathへ遷移するボタンの削除＋ルーティングの削除

--- app/controllers/tasks/sorts_controller.rb
+++ app/controllers/tasks/sorts_controller.rb
@@ -1,10 +1,10 @@
 class Tasks::SortsController < ApplicationController
   def update
     @task    = Task.find(params[:task_id])
+    @task.insert_at(params[:new_index].to_i + 1)
+
     @routine = @task.routine
     @tasks   = @routine.tasks.includes(:tags)
     @tags    = Tag.includes(:tasks)
-
-    @task.insert_at(params[:new_index].to_i + 1)
   end
 end
