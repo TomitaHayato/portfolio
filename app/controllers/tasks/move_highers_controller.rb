1b3a3920156710891b0175d3fd0e26485c4af4d1
hayato_52b <a99246780@icloud.com>
2024-12-07 19:59:53 +0900

    update: 並べ替えボタンによるタスクのソートをAjax

--- app/controllers/tasks/move_highers_controller.rb
+++ app/controllers/tasks/move_highers_controller.rb
@@ -1,8 +1,10 @@
 class Tasks::MoveHighersController < ApplicationController
   def update
-    task = Task.find(params[:task_id])
+    task    = Task.find(params[:task_id])
     task.move_higher
-    routine = task.routine
-    redirect_to routine_path(routine)
+
+    @routine = task.routine
+    @tasks   = @routine.tasks.includes(:tags)
+    @tags    = Tag.includes(:tasks)
   end
 end
