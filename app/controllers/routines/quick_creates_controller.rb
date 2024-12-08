7748bb1297a371836e3a89aacc1e6807a0eb6303
hayato_52b <a99246780@icloud.com>
2024-12-08 15:43:47 +0900

    ADD:ルーティンを1クリックで作成する機能を実装

---
+++ app/controllers/routines/quick_creates_controller.rb
@@ -0,0 +1,17 @@
+class Routines::QuickCreatesController < ApplicationController
+  def create
+    if current_user.quick_routine_template.nil?
+      current_user.create_quick_routine_template!
+    end
+
+    routine_template = current_user.quick_routine_template
+    new_routine      = current_user.routines.quick_build(routine_template)
+
+    if new_routine.save
+      redirect_to routine_path(new_routine), notice: "ルーティンを作成しました"
+    else
+      flash.now[:alert] = "ルーティンの作成に失敗しました"
+      render turbo_stream: turbo_stream.update('flash', partial: "shared/flash")
+    end
+  end
+end
