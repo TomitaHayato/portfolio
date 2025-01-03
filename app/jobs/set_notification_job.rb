f6afd06800409806e6676070488ba199de774f25
hayato_52b <a99246780@icloud.com>
2025-01-03 23:53:31 +0900

    ADD: 通知Jobのエラーハンドリングを追加

--- app/jobs/set_notification_job.rb
+++ app/jobs/set_notification_job.rb
@@ -1,24 +1,28 @@
 class SetNotificationJob < ApplicationJob
   queue_as :default
+  sidekiq_options retry: false # Jobが失敗した際、再試行せず破棄
 
   # 全てのLINE登録済みユーザーに対し、ルーティンの開始時間にLINE Push通知を送信するJobを呼び出す
   def perform
     time_now = Time.current
+    begin
+      User.includes(:authentications).find_each do |user|
+        active_routine = user.routines.find_by(is_active: true)
 
-    User.includes(:authentications).find_each do |user|
-      active_routine = user.routines.find_by(is_active: true)
+        next if active_routine.nil? || user.off? || !start_time?(active_routine.start_time, time_now)
 
-      next if active_routine.nil? || user.off? || !start_time?(active_routine.start_time, time_now)
+        case user.notification
+        when 'line'
+          next unless user.link_line? # UserがLineを介して登録していない場合はスキップ
 
-      case user.notification
-      when 'line'
-        next unless user.link_line? # UserがLineを介して登録していない場合はスキップ
-
-        uid = user.authentications.find_by(provider: 'line', user_id: user.id).uid
-        LineNotificationJob.perform_later(uid)
-      when 'email'
-        NotificationMailer.with(user:).notify_email.deliver_later
+          uid = user.authentications.find_by(provider: 'line', user_id: user.id).uid
+          LineNotificationJob.perform_later(uid)
+        when 'email'
+          NotificationMailer.with(user:).notify_email.deliver_later
+        end
       end
+    rescue StandardError => e
+      Rails.logger.error("通知Jobは失敗しました。エラー: #{e.message}")
     end
   end
 
