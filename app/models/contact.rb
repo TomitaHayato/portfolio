28905c8a92e1888583f5417dbeafdf4463b4d883
hayato_52b <a99246780@icloud.com>
2025-01-03 16:11:39 +0900

    ADD: Contactsテーブル、モデルを作成

---
+++ app/models/contact.rb
@@ -0,0 +1,9 @@
+class Contact < ApplicationRecord
+  validates :name   , presence: true
+  validates :email  , presence: true
+  validates :subject, presence: true
+  validates :message, presence: true
+
+  enum subject:          { bug: 0, improvement: 1, others: 2 }
+  enum is_need_response: {  no: 0,         yes: 1 }
+end
