60a8bd841a3a11db5d96535eb0e71b8ffcfa37de
hayato_52b <a99246780@icloud.com>
2025-01-08 00:32:24 +0900

    FIX: ログイン処理時に、ルーティンテンプレートを作成する処理を実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,6 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
+      @user.create_quick_routine_template! unless @user.quick_routine_template
       flash[:notice] = 'ログインしました！'
       redirect_to my_pages_path
     else
