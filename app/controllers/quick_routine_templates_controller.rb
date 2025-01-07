5c2694a29ef075c7f2986879e2b6250597f4ed7f
hayato_52b <a99246780@icloud.com>
2025-01-07 20:32:55 +0900

    ADD: 1click生成のテンプレートを編集する機能を実装

---
+++ app/controllers/quick_routine_templates_controller.rb
@@ -0,0 +1,26 @@
+class QuickRoutineTemplatesController < ApplicationController
+  before_action :is_create_template
+
+  def edit
+    @quick_routine_template = current_user.quick_routine_template
+  end
+
+  def update
+    @quick_routine_template = current_user.quick_routine_template
+
+    redirect_to routines_path, notice: '1click作成用テンプレートを更新しました' if @quick_routine_template.update(quick_routine_template_params)
+  end
+
+  private
+
+  def quick_routine_template_params
+    params.require(:quick_routine_template).permit(:title, :description, :start_time)
+  end
+
+  # Routineテンプレートがない場合作成
+  def is_create_template
+    return if current_user.quick_routine_template
+
+    current_user.create_quick_routine_template!
+  end
+end
