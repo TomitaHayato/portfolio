fcb7ac73a8b6f248a238cbff28bb663a5cb69930
hayato_52b <a99246780@icloud.com>
2025-01-07 18:46:32 +0900

    Update: 不要なヘルパーの削除＋コードの整形

--- app/controllers/routines/actives_controller.rb
+++ app/controllers/routines/actives_controller.rb
@@ -1,10 +1,12 @@
 class Routines::ActivesController < ApplicationController
   def update
-    @routine            = current_user.routines.find(params[:routine_id])
-    @routine_active_now = current_user.routines.find_by(is_active: true)
-    return if @routine == @routine_active_now
+    @routine = current_user.routines.find(params[:routine_id])
+    return if @routine.is_active?
 
+    # 元々activeだったルーティンを非アクティブ化
+    @routine_active_now = current_user.routines.find_by(is_active: true)
     @routine_active_now&.update!(is_active: false)
+
     @routine.update!(is_active: true)
   end
 end
