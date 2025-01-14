462969333fb81862cc49b88c32644299f8078117
hayato_52b <a99246780@icloud.com>
2025-01-14 20:41:56 +0900

    Add: 週間レポートをプロフィールに追加

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -17,7 +17,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   def default_url
-    'user_icon.jpg'
+    'logo.jpg'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
